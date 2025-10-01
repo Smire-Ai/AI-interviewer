fa3ebcd602f2726271a2b591dcbf291645183668
Ahmed <ahmedzaweel@gmail.com>
2025-10-01 18:08:02 +0530

    starting rom resh

--- ai_interview_platform/admin_panel/admin_project/core/admin.py
+++
@@ -1,8 +0,0 @@
-# File: admin_panel/core/admin.py
-from django.contrib import admin
-from .models import Users, Candidates, CandidateResumes
-
-# This tells Django to show these tables in the admin interface
-admin.site.register(Users)
-admin.site.register(Candidates)
-admin.site.register(CandidateResumes)
